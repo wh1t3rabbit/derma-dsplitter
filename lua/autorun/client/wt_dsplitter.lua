2d1a20d60610cd0e5e65e41b95aa4319cf0cb609
wh1t3rabbit <wh1t3rabbit@users.noreply.github.com>
2015-07-18 16:33:02 -0700

    Now it works correctly
    Fix initial mistakes in untested code.

--- lua/autorun/client/wt_dsplitter.lua
+++ lua/autorun/client/wt_dsplitter.lua
@@ -5,16 +5,19 @@ do --DSplitter container
 	-- A panel that contains two content panels with a bar between them for resizing.
 	-- @author Whiterabbit facepunch.com/member.php?u=468356
 	local PNL = {}
+	local BaseClass = baseclass.Get("DPanel")
 	
 	function PNL:Init()
-		self.Panel1 = self:Add("DPanel")
+		self.Panel1 = self:Add("DSplitterPanel")
 		self.Splitter = self:Add("DSplitterBar")
-		self.Panel2 = self:Add("DPanel")
+		self.Panel2 = self:Add("DSplitterPanel")
 		self:SetPanelColor(color_white,1)
 		self:SetPanelColor(color_white,2)
-		self:SetSplitterColor(Color(128,128,128))
-		self:SetSplitterThickness(4)
-		self:SetStretchPanel(1)
+		self:SetSplitterColor(Color(60,60,60))
+		self:SetSplitterThickness(8)
+		self:SetStretchPanel(2)
+		self:SetFixedPanelMinSize(50)
+		self:SetFixedPanelMaxSize(-1)
 		self:SetHorizontal()
 	end
 	
@@ -22,6 +25,18 @@ do --DSplitter container
 	-- @param pnlnum Which panel will expand (1 or 2).
 	function PNL:SetStretchPanel(pnlnum)
 		self.StretchPanel = (pnlnum<=1) and 1 or 2
+	end
+	
+	--- Set the minimum size of the non-expanding panel.
+	-- @param minwidth
+	function PNL:SetFixedPanelMinSize(minwidth)
+		self.FixedPanelMinSize = minwidth
+	end
+	
+	--- Set the maximum size of the non-expanding panel.
+	-- @param maxwidth
+	function PNL:SetFixedPanelMaxSize(maxwidth)
+		self.FixedPanelMaxSize = maxwidth
 	end
 	
 	--- Set container to layout horizontally.
@@ -45,20 +60,24 @@ do --DSplitter container
 				self.Panel1:Dock(FILL)
 				self.Splitter:Dock(BOTTOM)
 				self.Panel2:Dock(BOTTOM)
+				self.Splitter:MoveToAfter(self.Panel2)
 			else
 				self.Panel1:Dock(TOP)
 				self.Splitter:Dock(TOP)
 				self.Panel2:Dock(FILL)
+				self.Splitter:MoveToBefore(self.Panel2)
 			end
 		else
 			if self.StretchPanel==1 then
 				self.Panel1:Dock(FILL)
 				self.Splitter:Dock(RIGHT)
 				self.Panel2:Dock(RIGHT)
+				self.Splitter:MoveToAfter(self.Panel2)
 			else
 				self.Panel1:Dock(LEFT)
 				self.Splitter:Dock(LEFT)
 				self.Panel2:Dock(FILL)
+				self.Splitter:MoveToBefore(self.Panel2)
 			end
 		end
 	end
@@ -87,42 +106,95 @@ do --DSplitter container
 		end
 	end
 	
+	function PNL:PerformLayout()
+		self.PerformingLayout = true
+			self:ResizeEx(0)
+		self.PerformingLayout = false
+		BaseClass.PerformLayout(self)
+	end
+	
 	--- Resize the panels when the splitter bar 'moves'.
 	-- Called internally by the DSplitterBar:Think().
+	-- Call it with 0 if you want to force a layout update.
 	-- @param nAmt Distance the bar tried to move.
 	function PNL:ResizeEx(nAmt)
 		if self.IsVertical then
 			if self.StretchPanel==1 then
-				local NewSize = self.Panel2:GetTall()+nAmt
-				if NewSize<=1 then
-					NewSize = 1
+				local NewSize = self.Panel2:GetTall()-nAmt
+				if NewSize < self.FixedPanelMinSize then
+					NewSize = self.FixedPanelMinSize
+				end
+				if self.FixedPanelMaxSize>=self.FixedPanelMinSize and self.FixedPanelMaxSize<NewSize then
+					NewSize = self.FixedPanelMaxSize
+				end
+				if NewSize > self:GetTall()-self.SplitterWidth then
+					NewSize = self:GetTall()-self.SplitterWidth
 				end
 				self.Panel2:SetTall( NewSize )
 			else
 				local NewSize = self.Panel1:GetTall()+nAmt
-				if NewSize<=1 then
-					NewSize = 1
+				if NewSize < self.FixedPanelMinSize then
+					NewSize = self.FixedPanelMinSize
+				end
+				if self.FixedPanelMaxSize>=self.FixedPanelMinSize and self.FixedPanelMaxSize<NewSize then
+					NewSize = self.FixedPanelMaxSize
+				end
+				if NewSize > self:GetTall()-self.SplitterWidth then
+					NewSize = self:GetTall()-self.SplitterWidth
 				end
 				self.Panel1:SetTall( NewSize )
 			end
-		elseif self.IsHorizontal then
+		else
 			if self.StretchPanel==1 then
-				local NewSize = self.Panel2:GetWide()+nAmt
-				if NewSize<=1 then
-					NewSize = 1
+				local NewSize = self.Panel2:GetWide()-nAmt
+				if NewSize < self.FixedPanelMinSize then
+					NewSize = self.FixedPanelMinSize
+				end
+				if self.FixedPanelMaxSize>=self.FixedPanelMinSize and self.FixedPanelMaxSize<NewSize then
+					NewSize = self.FixedPanelMaxSize
+				end
+				if NewSize > self:GetWide()-self.SplitterWidth then
+					NewSize = self:GetWide()-self.SplitterWidth
 				end
 				self.Panel2:SetWide( NewSize )
 			else
 				local NewSize = self.Panel1:GetWide()+nAmt
-				if NewSize<=1 then
-					NewSize = 1
+				if NewSize < self.FixedPanelMinSize then
+					NewSize = self.FixedPanelMinSize
+				end
+				if self.FixedPanelMaxSize>=self.FixedPanelMinSize and self.FixedPanelMaxSize<NewSize then
+					NewSize = self.FixedPanelMaxSize
+				end
+				if NewSize > self:GetWide()-self.SplitterWidth then
+					NewSize = self:GetWide()-self.SplitterWidth
 				end
 				self.Panel1:SetWide( NewSize )
 			end
 		end
+		if not self.PerformingLayout then
+			--Immediately do layout so docking can stretch us correctly. If not immediate there is a visible gap while resizing.
+			self.Panel1:InvalidateLayout(true)
+			self.Panel2:InvalidateLayout(true)
+			self:InvalidateLayout(true)
+		end
 	end
 	
 	vgui.Register("DSplitter",PNL,"DPanel")
+end
+
+do --DSplitter panel
+
+	--- DSplitterPanel.
+	-- Just override DPanel to use the specified background color.
+	local PNL = {}
+	
+	function PNL:Paint(w,h)
+		surface.SetDrawColor(self:GetBackgroundColor())
+		self:DrawFilledRect()
+	end
+	
+	vgui.Register("DSplitterPanel", PNL, "DPanel")
+
 end
 
 do --DSplitter bar
@@ -163,11 +235,14 @@ do --DSplitter bar
 		if self.Sizing then
 			local mousex,mousey = gui.MouseX(),gui.MouseY()
 			local dx,dy = mousex-self.Sizing[1],mousey-self.Sizing[2]
-			self:SetCursor( self.IsTall and "sizewe" or "sizens" )
-			self:GetParent():ResizeEx(IsTall and dx or dy)
+			self:GetParent():ResizeEx(self.IsTall and dx or dy)
 			self.Sizing[1],self.Sizing[2] = mousex,mousey
-		elseif self.Hovered then
 			self:SetCursor( self.IsTall and "sizewe" or "sizens" )
+			return
+		end
+		if self.Hovered then
+			self:SetCursor( self.IsTall and "sizewe" or "sizens" )
+			return
 		end
 		self:SetCursor("arrow")
 	end
